482f503a854b2595b4c9ddd8c911eb66d69fc6ae
Aseem Kohli <aseemko@gmail.com>
2023-02-25 17:42:08 -0500

    add recipe model and recipe list

--- angular/recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular/recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://images.pexels.com/photos/1556688/pexels-photo-1556688.jpeg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://images.pexels.com/photos/1556688/pexels-photo-1556688.jpeg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
